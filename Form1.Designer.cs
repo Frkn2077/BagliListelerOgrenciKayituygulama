dcacb26e37e5fca78d5a52453eead0d33f5f414a
Frkn2077 <159815236+Frkn2077@users.noreply.github.com>
2025-01-17 17:14:57 +0300

    proje dosyaları yüklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Bağlı_liste_uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(297, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(297, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Okul numarası ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(428, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Başa Eleman ekle ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(428, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sona eleman ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Araya  eleman ekleme";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(444, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 34);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Düğüm sil";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(651, 156);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 59);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "YAZDIR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(-3, 331);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(801, 116);
+            this.listBox1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "DÜĞÜM UYGULMASI";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Bağlı_liste_uygulama
+{
+    public partial class Form1 : Form
+    {
+       private BagliListe bl = new BagliListe();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String m1 = textBox1.Text;
+            String m2 = textBox2.Text;
+            
+            int k = Convert.ToInt32(textBox3.Text);
+            Dugum d1 = new Dugum(k,m1,m2);
+            bl.ArayaEkle(d1.Veri,d1.Ad,d1.Soyad);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            String m1 = textBox1.Text;
+            String m2 = textBox2.Text;
+            int s1=Convert.ToInt32(textBox3.Text);
+            Dugum d1 = new Dugum(s1,m1,m2);
+            bl.dugumuSil(d1.Veri);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String m1 = textBox1.Text;
+            String m2 = textBox2.Text;
+            int k = Convert.ToInt32(textBox3.Text);
+            Dugum d1 = new Dugum(k,m1,m2);
+            bl.sonaEkle(d1.Veri,d1.Ad,d1.Soyad);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //başa eleman ekleme
+                String m1 = textBox1.Text;
+                String m2 = textBox2.Text;
+                int k = Convert.ToInt32(textBox3.Text);
+                Dugum d1 = new Dugum(k,m1,m2);
+                bl.BasaEkle(d1.Veri, d1.Ad, d1.Soyad);
+
+           
+        }
+        // Düğüm sınıfı (Node)
+        public class Dugum
+        {
+            public string Ad { get; set; }
+            public string Soyad { get; set; }
+            public int Veri{ get; set; } // Düğümde saklanacak veri
+            public Dugum Sonraki{ get; set; } // Bir sonraki düğüme referans
+
+            // Yapıcı fonksiyon (Constructor)
+            public Dugum(int veri,string ad,string soyad)
+            {
+                this.Ad = ad;
+                this.Soyad = soyad;
+                this.Veri = veri;
+                this.Sonraki = null; // Başlangıçta bir sonraki düğüm yok
+            }
+        }
+
+        // Tek yönlü bağlı liste sınıfı
+        public class BagliListe
+        {
+            private Dugum baslangic; // Listenin başını (head) tutar
+            
+            public BagliListe()
+            {
+                baslangic = null; // İlk başta liste boş
+            }
+         
+            public void Yazdir(ListBox listBox)
+            {
+                listBox.Items.Clear(); // Mevcut öğeleri temizle
+                Dugum temp = baslangic; // Listeyi baştan dolaşmaya başla
+                String m1 = "";
+                while (temp != null)
+                {
+                    m1 += $"No: {temp.Veri}, Ad: {temp.Ad}, Soyad: {temp.Soyad}\n"; // Ad, Soyad ve No'yu yazdır ;
+
+                    temp = temp.Sonraki; // Sonraki düğüme geç
+                }
+                listBox.Items.Add(m1);// Veriyi ListBox'a ekle
+            }
+
+            // Listenin başına düğüm ekleme fonksiyonu
+            public void BasaEkle(int veri,string ad,string soyad)
+            {
+                Dugum yeniDugum = new Dugum(veri,ad,soyad); // Yeni düğüm oluştur
+                yeniDugum.Sonraki = baslangic; // Yeni düğümün 'Sonraki' kısmı mevcut başa işaret eder
+                baslangic = yeniDugum; // Yeni düğümü baş olarak ayarla
+                
+            }
+            public void ArayaEkle(int data,string ad,string soyad)
+            {
+                Dugum newNode = new Dugum(data, ad, soyad);
+
+                // Eğer liste boşsa
+                if (baslangic == null)
+                {
+                    baslangic= newNode;
+                }
+                else
+                {
+                    Dugum current = baslangic;
+                    while (current.Sonraki != null && newNode.Veri > current.Sonraki.Veri)
+                    {
+                        current = current.Sonraki;
+                    }
+
+                    
+                        newNode.Sonraki = current.Sonraki;
+                        current.Sonraki = newNode;
+                    
+                }
+            }
+
+
+            // Araya düğüm ekleme fonksiyonu (belirli bir değerden sonra)
+
+            public void sonaEkle(int veri,string ad, string soyad)
+            {
+                Dugum yeniDugum = new Dugum(veri,ad, soyad); // Yeni düğüm oluştur
+
+                // Eğer liste boşsa, yeni düğüm doğrudan başa eklenir
+                if (baslangic == null)
+                {
+                    baslangic = yeniDugum;
+                }
+                else
+                {
+                    // Listenin sonuna gitmek için geçici bir düğüm kullanılır
+                    Dugum gecici = baslangic;
+                    while (gecici.Sonraki != null)
+                    {
+                        gecici = gecici.Sonraki;
+                    }
+                    // Son düğüm bulunduğunda, onun 'sonraki' kısmı yeni düğüme işaret eder
+                    gecici.Sonraki = yeniDugum;
+                }
+                
+
+
+            }
+           
+
+
+            public void dugumuSil(int silinecekDeger)
+            {
+                // Liste boşsa hiçbir şey yapma
+                if (baslangic == null)
+                {
+                    return;
+                }
+
+                // Silinecek düğüm baştaki düğümse
+                if (baslangic.Veri == silinecekDeger)
+                {
+                    baslangic = baslangic.Sonraki;
+                    return;
+                }
+
+                // Listede dolanmak için geçici düğümler
+                Dugum gecici = baslangic;
+                Dugum onceki = null;
+
+                // Silinecek düğüm bulunana kadar döngü
+                while (gecici != null && gecici.Veri != silinecekDeger)
+                {
+                    onceki = gecici;
+                    gecici = gecici.Sonraki;
+                }
+
+                // Eğer düğüm bulunduysa sil
+                if (gecici != null)
+                {
+                    onceki.Sonraki = gecici.Sonraki;
+                }
+                else
+                {
+                    MessageBox.Show("Silinecek değer listede bulunamadı.");
+                }
+            }
+
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String m1=textBox1.Text;
+            String m2=textBox2.Text;
+            int s1=Convert.ToInt32(textBox3.Text);
+            Dugum d1 = new Dugum(s1,m1,m2);
+           
+            bl.sonaEkle(d1.Veri,d1.Ad, d1.Soyad);
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bl.Yazdir(listBox1);
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
